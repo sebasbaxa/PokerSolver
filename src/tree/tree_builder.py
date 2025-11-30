2726493700318ffb36c1db1c69cbb494ac619813
Sebastian Baxa <sebasbaxa@gmail.com>
2025-11-30 01:39:54 -0500

    create root node method and test

--- src/tree/tree_builder.py
+++ src/tree/tree_builder.py
@@ -15,20 +15,23 @@ class TreeBuilder:
         # Create unique IDs for all combinations of player hands in their ranges
         ids = create_ids(self.oop_range, self.ip_range)
         for hand_id in ids:
+
+            # determine player
             if hand_id.startswith("OOP"):
                 player = 'OOP'
-                hand_str = hand_id
             else:
                 player = 'IP'
-                hand_str = hand_id
-
-            hand = get_hand_from_id(hand_str)
-
+            
+            # find correct opponent range
             if player == 'OOP':
                 opponent_range = self.ip_range
             else:
                 opponent_range = self.oop_range
 
+            # reconstruct hand from id
+            hand = get_hand_from_id(hand_id)
+
+            # create gamestate for this hand at root node
             gamestate = GameState(player, hand, opponent_range)
             if player == 'OOP':
                 gamestate.player_stack = OOP_stack
@@ -41,7 +44,10 @@ class TreeBuilder:
                 gamestate.opponent_stack = OOP_stack
                 gamestate.opponent_contribution = OOP_contribution
 
+            # add the new state to the node's states dictionary
             root.states[hand_id] = gamestate
+        
+        return root
 
         
 
--- tests/test_tree_build.py
+++ tests/test_tree_build.py
@@ -3,8 +3,25 @@ from src.tree.tree_builder import TreeBuilder
 from src.game.player_range import PlayerRange
 
 
-class TestTreeBuilder(unittest.TestCase):
-    pass
+class TestCreateRootNode(unittest.TestCase):
 
+    # Test that the root node is being created correctly with the correct number of gamestates
+    def test_create_root_node(self):
+        oop_range = PlayerRange('OOP')
+        oop_range.add_pocket_pair('A')  # AA
 
-# TODO: Add specific tests for TreeBuilder methods
+        ip_range = PlayerRange('IP')
+        ip_range.add_suited_cards('K', 'Q')  # KQs
+
+        tree_builder = TreeBuilder(oop_range, ip_range)
+        root_node = tree_builder.create_root_node(
+            OOP_stack=1000,
+            IP_stack=1000,
+            OOP_contribution=50,
+            IP_contribution=50,
+            pot=100
+        )
+        
+        self.assertIsNotNone(root_node)
+        self.assertEqual(len(root_node.states), 10)  # 6 (AA) + 4 (KQs) = 10 unique hands
+
